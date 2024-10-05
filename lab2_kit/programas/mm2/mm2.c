21956bd33e5d3a4a71cdc32e1d403958257547ac
Quintino <davidkintino@gmail.com>
2024-10-05 18:18:38 +0100

    mm2 with transpose in time

--- lab2_kit/programas/mm2/mm2.c
+++ lab2_kit/programas/mm2/mm2.c
@@ -16,12 +16,12 @@ void transpose(int16_t m[N][N], int16_t res[N][N]) {
 }
 
 void setup(int16_t m1[N][N], int16_t m2[N][N], int16_t m3[N][N]) {
-    int16_t tmp[N][N];
+//    int16_t tmp[N][N];
     memset(m3, 0, sizeof(int16_t) * N * N);
     for (size_t i = 0; i < N; ++i) {
         for (size_t j = 0; j < N; ++j) {
             m1[i][j] = (i + j) % 8 + 1;
-            tmp[i][j] = (N - i + j) % 8 + 1;
+            m2[i][j] = (N - i + j) % 8 + 1;
         }
     }
 
@@ -29,17 +29,20 @@ void setup(int16_t m1[N][N], int16_t m2[N][N], int16_t m3[N][N]) {
     /*      MATRIX TRANSPOSITION        */
     /************************************/
 
-    transpose(tmp, m2);
+//    transpose(tmp, m2);
 }
 
 
 
 void multiply_matrices(int16_t const factor1[N][N], int16_t const factor2[N][N],
                        int16_t res[N][N]) {
+    int16_t tmp[N][N];                    
+    transpose(factor2, tmp);     
+
     for (size_t i = 0; i < N; ++i) {
         for (size_t j = 0; j < N; ++j) {
             for (size_t k = 0; k < N; ++k) {
-                res[i][j] += factor1[i][k] * factor2[j][k];
+                res[i][j] += factor1[i][k] * tmp[j][k];
             }
         }
     }
